a08d94bb2d8c3bc8fddd4e592e8fb7722660531c
silviawolves <silviacanepa.w@gmail.com>
2022-05-31 12:59:28 +0200

    codice base + consegna

---
+++ script.js
@@ -0,0 +1,17 @@
+'use strict'
+
+
+
+
+
+
+
+
+/*
+Descrizione:
+Visualizzare in pagina (html) 5 numeri casuali.
+Avviare un timer di 30 secondi
+Dopo 30 secondi, nascondere i numeri.
+L’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite un prompt().
+Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+*/

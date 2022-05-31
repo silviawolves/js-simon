c6c61c75622f2a270e71e0d22fdc9d4eb3109ad9
silviawolves <silviacanepa.w@gmail.com>
2022-05-31 14:49:38 +0200

    conto alla rovescia + prompt timeout

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ const numeriCasuali = []
 //uno che prenda i numeri inseriti dall'utente
 const numeriUtente = []
 
+let partitaVinta = false
+
 //creo un ciclo per generare cinque numeri random
 for (let i = 0; i < 5; i++) {
     const numeroRandom = Math.floor((Math.random() * 50) + 1)
@@ -26,12 +28,11 @@ for (let i = 0; i < 5; i++) {
 //appendo i numeri casuali all'html
 contenitoreNumeri.innerHTML = numeriCasuali.join(' - ')
 
-//creo una costante per l'intervallo, se dovessi usarla dopo
-let contoAllaRovescia;
+//creo una costante per i secondi totali
 let secondiRestanti = 30
 
 //creo la funzione per il countdown di trenta secondi
-contoAllaRovescia = setInterval(function() {
+const contoAllaRovescia = setInterval(function() {
     
     secondiRestanti--;
     const secondi = secondiRestanti % 60
@@ -42,19 +43,39 @@ contoAllaRovescia = setInterval(function() {
     //se arriva a 00:00 il conto alla rovescia si blocca
     if (secondiRestanti === 0) {
         clearInterval(contoAllaRovescia)
+        contenitoreNumeri.classList.add('d-none')
     }
 }, 1000)
 
+setTimeout(function () {
 
+//dopo i trenta secondi, appaiono i prompt per inserire i numeri visti
+    //function partitaNumeri() {
 
+        for (let i = 0; i < 5; i++) {
+            const numeriScelti = +prompt('Adesso scrivi i numeri che hai visto.')
 
+            if (!isNaN(numeriScelti) ) {
+                numeriUtente.push(numeriScelti)
+            } else {
+                i--
+            }
+        }
+        console.log(numeriUtente)
+
+        //analizzo array numeri utente, per vedere se i suoi numeri sono nell'array casuale
+        for (let i = 0; i <= numeriUtente.length; i++) {
+        }
+    //}
+
+}, 35000)
 
 
 /*
 Descrizione:
 Visualizzare in pagina (html) 5 numeri casuali. x
-Avviare un timer di 30 secondi
-Dopo 30 secondi, nascondere i numeri.
-L’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite un prompt().
+Avviare un timer di 30 secondi x
+Dopo 30 secondi, nascondere i numeri. x
+L’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite un prompt(). x
 Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 */
